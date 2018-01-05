a952d649844907a742a9c5c9a9e176680bf5a7eb
Nick Selent <nickselent@yahoo.com>
2018-01-04 21:39:48 -0500

    first push

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BobNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sweep sweep;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            sweep = new Sweep(MainCanvas, RadarScreen);
+            DispatcherTimer t = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(Framejack.Value)
+            };
+            t.Tick += FrameTick;
+            t.Start();
+        }
+
+        private void FrameTick(object sender, EventArgs e)
+        {
+            sweep.Move();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ Sweep.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BobNet
+{
+    public class Sweep
+    {
+        Line line = new Line();
+        Canvas canvas;
+        Ellipse radarScreen;
+
+
+        double X1 = 0;
+        double Y1 = 0;
+
+        int centerPoint;
+        int u;
+
+        public Sweep(Canvas mainCanvas, Ellipse screen)
+        {
+            canvas = mainCanvas;
+            radarScreen = screen;
+            Add();
+        }
+
+        private void Add()
+        {
+            u = 26;
+            X1 = radarScreen.Width / 2;
+            Y1 = radarScreen.Height / 2;
+
+            centerPoint = (int)radarScreen.Width / 2;
+
+            line.X1 = X1;
+            line.Y1 = Y1;
+            line.X2 = X1 + (int)(centerPoint * Math.Sin(Math.PI * u / 180));
+            line.Y2 = Y1 - (int)(centerPoint * Math.Cos(Math.PI * u / 180));
+
+            SolidColorBrush greenBrush = new SolidColorBrush()
+            {
+                Color = Colors.Green
+            };
+            line.StrokeThickness = 1;
+            line.Stroke = greenBrush;
+
+            canvas.Children.Add(line);
+        }
+
+        public void Move()
+        {
+            u++;
+            line.X2 = X1 + (int)(centerPoint * Math.Sin(Math.PI * u / 180));
+            line.Y2 = Y1 - (int)(centerPoint * Math.Cos(Math.PI * u / 180));
+        }
+    }
+}
